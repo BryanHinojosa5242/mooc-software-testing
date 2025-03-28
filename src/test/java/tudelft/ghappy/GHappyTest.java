f43abd32ce13e05ba6b359754ebe946ce6512c80
Bryan Hinojosa <bryan.hinojosa@ucsm.edu.pe>
2025-03-27 21:11:26 -0500

    Creada pruebas para verificar la funcionalidad de GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void testGHappy_AllGsHappy() {
+        GHappy gh = new GHappy();
+        Assertions.assertTrue(gh.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void testGHappy_SingleGUnhappy() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void testGHappy_UnhappyGAtEnd() {
+        GHappy gh = new GHappy();
+        Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+    }
 }
